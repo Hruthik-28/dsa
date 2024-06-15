a31a661bdce4ffe34b02e33c8d2f8259b4bf5a46
HRUTHIK <hruthik9980819630@gmail.com>
2024-06-15 10:17:38 +0530

    recurssion for balaance parantheses

--- 05_recurssion/13_balance_parantheses.js
+++ 05_recurssion/13_balance_parantheses.js
@@ -5,9 +5,13 @@ function balanceParantheses(arr) {
     const stack = [];
 
     for (const parantheses of arr) {
-        if (parantheses === "(") {
+        if (parantheses === "(" || parantheses === "{" || parantheses === "[") {
             stack.push(parantheses);
-        } else if (parantheses === ")") {
+        } else if (
+            parantheses === ")" ||
+            parantheses === "}" ||
+            parantheses === "]"
+        ) {
             if (stack.length === 0) {
                 return false;
             }
@@ -18,4 +22,28 @@ function balanceParantheses(arr) {
     return stack.length === 0;
 }
 
-console.log(balanceParantheses(["(", "(", ")", ")", "(", ")"])); // true
+console.log(balanceParantheses(["(", "{", "}", "[", "]", ")"])); // true
+console.log(balanceParantheses(["(", "{", "}", "[", "[", ")"])); // false
+
+// recursive
+function recBalanceParantheses(arr, startIndex = 0, count = 0) {
+    if (startIndex === arr.length) {
+        return count === 0;
+    }
+
+    if (count < 0) {
+        return false;
+    }
+
+    if (arr[startIndex] == "(") {
+        return balanceParantheses(arr, startIndex + 1, count);
+    } else if (arr[startIndex == ")"]) {
+        return balanceParantheses(arr, startIndex + 1, count);
+    } else {
+        return false;
+    }
+}
+
+console.log(recBalanceParantheses(["(", "(", ")", ")", "(", ")"])); // true
+console.log(recBalanceParantheses(["(", "{", "}", "[", "]", ")"])); // true
+console.log(recBalanceParantheses(["(", "{", "}", "[", "[", ")"])); // false
